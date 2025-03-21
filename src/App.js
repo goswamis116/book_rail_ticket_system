b0ac145cdf5f953c8543fd4fcba994a212ac3339
bandhan\183263 <183263@bandhanbank.com>
2025-03-21 17:45:26 +0530

    Train list page added 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import ShowTrains from './components/ShowTrains';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/show-trains" element={<ShowTrains />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
